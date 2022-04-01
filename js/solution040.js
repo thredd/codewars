a891953a2c4e14a8148cb4c4c2f7ddd4145c092a
Nikki <n.thredd@gmail.com>
2022-04-01 10:32:31 -0400

    completed 40

---
+++ js/solution040.js
@@ -0,0 +1,9 @@
+// For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre
+
+var quote = function(fighter) {
+    if(fighter.toLowerCase() === 'george saint pierre'){
+      return "I am not impressed by your performance."
+    } else if (fighter.toLowerCase() === 'conor mcgregor'){
+      return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+    }
+  }
