375e3e44c579be9ca6ee1126821ed5d56a6a7708
Nikki <n.thredd@gmail.com>
2022-04-07 10:27:46 -0400

    completed 35

---
+++ js/solution035.js
@@ -0,0 +1,13 @@
+// Define a card suit
+
+function defineSuit(card) {
+    if(card.includes('♣')){
+      return 'clubs'
+    } else if(card.includes('♦')){
+      return 'diamonds'
+    } else if(card.includes('♥')){
+      return 'hearts'
+    } else if(card.includes('♠')){
+      return 'spades'
+    }
+  }
