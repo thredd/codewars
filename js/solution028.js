6075277bed846b2dcc029edd78c5efc053bf20e0
Nikki <n.thredd@gmail.com>
2022-04-14 10:19:38 -0400

    completed 28

---
+++ js/solution028.js
@@ -0,0 +1,9 @@
+// Thinkful - Number Drills: Pixelart planning
+
+function isDivisible(wallLength, pixelSize){
+    if(wallLength % pixelSize === 0){
+      return true
+    } else {
+      return false
+    }
+  }
