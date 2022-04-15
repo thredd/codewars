4018e68d1dd86a83374abb4b40ecb6fd529a6caf
Nikki <n.thredd@gmail.com>
2022-04-15 10:18:21 -0400

    completed 27

---
+++ js/solution027.js
@@ -0,0 +1,9 @@
+// Fix the Bugs (Syntax) - My First Kata
+
+function myFirstKata(a,b) {
+    if (typeof a !== "number" || typeof b !== "number"){
+      return false
+    } else {
+      return a % b + b % a
+    }
+  }
