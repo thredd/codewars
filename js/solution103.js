46f77a8edee90cf4ff2e169669bd46477d795c6d
thredd <n.thredd@gmail.com>
2022-06-21 19:00:27 -0400

    completed 103

---
+++ js/solution103.js
@@ -0,0 +1,8 @@
+// Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right
+
+function remove(s,n){
+    for (let i = 0; i < n; i++){
+      s = s.replace("!","")
+    }
+    return s
+  }
